578a7e1081d9b7ff6b5a826478d8b5fb60528d2b
mtravison <149450878+mtravison@users.noreply.github.com>
2023-11-05 10:13:59 -0500

    Revert "Initial Update to the Program"
    This reverts commit ac672464682a7795abdebf089709b3fbf95d429c.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello World");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
+      system.out.println("Hello World");
+      }
    }
-}
+   }
